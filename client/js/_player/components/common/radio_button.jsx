665a648f79297bc7a5c094fdc8367e85ee5201bd
resource11 <mcmahon@resource11.com>
2017-11-16 16:38:25 -0500

    resets pointer cursor to auto on question save

--- client/js/_player/components/common/radio_button.jsx
+++ client/js/_player/components/common/radio_button.jsx
@@ -47,7 +47,7 @@ export default class RadioButton extends React.Component {
           htmlFor={id}
           key={id}
           className={isDisabled  // eslint-disable-line no-nested-ternary
-          ? 'c-answer-container'
+          ? 'c-answer-container--disabled'
           : ((focused && !isDisabled) || (checked && !isDisabled)
           ? 'c-answer-container is-focused'
           : 'c-answer-container')
